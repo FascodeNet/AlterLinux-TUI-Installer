e8556d9154df7ac94277f50af1faed14c73e0ad2
kokkiemouse <kokkiemouse@gmail.com>
2020-08-30 16:29:45 +0900

    fixed Par
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- install.sh
+++ install.sh
@@ -592,4 +592,20 @@ do_install()
 			;;
 	esac
 }
+
+auto_detect()
+{
+	tmp=/tmp/dev2mod
+	echo 'dev2mod() { while read dev; do case $dev in' > $tmp
+	sort -r /lib/modules/`uname -r`/modules.alias | \
+		sed -n 's/[()]/*/g; s/^alias  *\([^ ]*\)  *\(.*\)/\1)modprobe \2;;/p' >> $tmp
+	echo 'esac; done; }' >> $tmp
+	for f in $(grep -Eh "drm_kms|sound.core|hyperv" /lib/modules/`uname -r`/modules.dep | cut -d. -f1); do
+		sed -i "/$(basename $f | sed 's/-/_/g')/d" $tmp
+	done
+	source $tmp
+	cat /sys/bus/*/devices/*/uevent | grep MODALIAS | sed 's/^MODALIAS=//' | awk '!seen[$0]++' | dev2mod
+	cat /sys/devices/virtual/wmi/*/modalias | dev2mod
+}
+auto_detect
 do_install
