0f6e92bc7a5b8965fcde7ff713cdaaaec2b35bdc
kokkiemouse <kokkiemouse@gmail.com>
2020-09-02 16:04:55 +0900

    delete
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-dev_array=()
-for dev_t in `lsblk -pl -o NAME`
-do
- if [[ ${dev_t} =~ ^/dev/[hmnsv][dmv][0-9a-z]* ]]; then
-    dev_array+=(${dev_t})
- fi
-done
-echo ${dev_array[@]}
