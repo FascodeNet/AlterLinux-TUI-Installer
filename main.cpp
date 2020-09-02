86eeaefd0b08ee23edf75570b47b6b343a26cc9a
kokkiemouse <kokkiemouse@gmail.com>
2020-09-02 17:00:30 +0900

    del

--- main.cpp
+++
@@ -1,4 +0,0 @@
-#include "main.h"
-int main(int argc,char* argv[]){
-    return 0;
-}
--- main.h
+++
@@ -1,5 +0,0 @@
-#ifndef __MAIN_H_ALTERL_TUI_INSTALLER_AAAAAAAAAAA
-#define __MAIN_H_ALTERL_TUI_INSTALLER_AAAAAAAAAAA
-#include <cdk.h>
-int main(int argc,char* argv[]);
-#endif
