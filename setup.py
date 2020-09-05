adb0d8c36671c8715ac6b1cef29e05a3704a8059
kokkiemouse <kokkiemouse@gmail.com>
2020-09-05 11:44:00 +0900

    fixed dep

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version="0.0.0.1",
     description="setup alterlinux with tui",
     author="FascodeNetwork",
-    install_requires=["dialog"],
+    install_requires=["pythondialog"],
     entry_points={
         "console_scripts":[
             "alterlinux_tui_installer=alterlinux_tui_installer.AlterLinux_TUI_Installer:main"
