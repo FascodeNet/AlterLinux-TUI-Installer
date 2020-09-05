0363bb61d061f1a31a0d17d365052b598e02fdce
kokkiemouse <kokkiemouse@gmail.com>
2020-09-05 11:40:36 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name="alterlinux_tui_installer",
+    version="0.0.0.1",
+    description="setup alterlinux with tui",
+    author="FascodeNetwork",
+    install_requires=["dialog"],
+    entry_points={
+        "console_scripts":[
+            "alterlinux_tui_installer=alterlinux_tui_installer.AlterLinux_TUI_Installer:main"
+        ]
+    }
+)
